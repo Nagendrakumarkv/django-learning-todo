3cc5d8eb85e65cdf16e154ea54c4cf4e6a181695
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2026-01-28 15:35:42 +0530

    Day 2 Goal: The "Read" Operation. We want to see our tasks in the Django Admin panel, and then on a public webpage

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Task  # Import the model
 
-# Create your views here.
+def task_list(request):
+    # 1. Fetch all tasks from the database
+    tasks = Task.objects.all()
+
+    # 2. Bundle them into a dictionary (context)
+    context = {'tasks': tasks}
+
+    # 3. Return the HTML page with the data inside
+    return render(request, 'tasks/task_list.html', context)
